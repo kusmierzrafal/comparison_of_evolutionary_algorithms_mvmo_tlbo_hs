2de71829edd5798061955452e45751ebb15a0b68
rafal.kusmierz.biz@gmail.com <r.kusmierz@stud.elka.pw.edu.pl>
2024-03-18 20:18:59 +0100

    mvmo another implementation comparison prepared

--- algorithms_comparison/mvmo_comparison/generate_comparison_populations.py
+++ algorithms_comparison/mvmo_comparison/generate_comparison_populations.py
@@ -1,24 +1,29 @@
-import numpy as np
-from evolutionary_algorithms.mvmo import MVMO
 import pickle
 import random
 
-np.random.seed(42)
-random.seed(42)
+import numpy as np
+from evolutionary_algorithms.mvmo import MVMO
 
-rosenbrock_boundaries = (-10, 10)
-dimensions = 6
-pop_sizes = [10, 40, 70]
-number_of_populations = 30
+from compare_mvmo_consts import (
+    DIMENSIONS,
+    POPULATIONS,
+    POPULATIONS_SIZES,
+    SEED,
+    TYPICAL_BOUNDARIES,
+)
+
+np.random.seed(SEED)
+random.seed(SEED)
 
 # prepare populations for comparison with another implementation
-mvmo_optimizer = MVMO(100_000, dimensions, rosenbrock_boundaries, False, mutation_size=1)
-for pop_size in pop_sizes:
-    for i in range(number_of_populations):
+# iterations don't make difference in this case
+mvmo_optimizer = MVMO(100_000, DIMENSIONS, TYPICAL_BOUNDARIES, False, mutation_size=1)
+for pop_size in POPULATIONS_SIZES:
+    for i in range(POPULATIONS):
         population = mvmo_optimizer.init_population(pop_size)
-        with open(f'./populations/init_pop_{pop_size}_nr_{i+1}', 'wb') as handle:
+        with open(f"./populations/init_pop_{pop_size}_nr_{i+1}", "wb") as handle:
             pickle.dump(population, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-# read population
+# example of reading population
 # with open('./populations/init_pop_10_nr_1', 'rb') as handle:
 #         mvmo_population = pickle.load(handle)
