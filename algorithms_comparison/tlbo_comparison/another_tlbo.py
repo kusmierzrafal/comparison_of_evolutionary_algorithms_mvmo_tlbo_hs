d4967442fe0ff8f02d9b57ca6a57cd451b3cb18a
rafal.kusmierz.biz@gmail.com <r.kusmierz@stud.elka.pw.edu.pl>
2024-03-20 21:44:04 +0100

    another tlbo implementation prepared for comparison

--- algorithms_comparison/tlbo_comparison/another_tlbo.py
+++ algorithms_comparison/tlbo_comparison/another_tlbo.py
@@ -5,7 +5,7 @@ import random
 import math  # cos() for Rastrigin
 import copy  # array-copying convenience
 import sys  # max float
-
+import numpy as np  # just for levy testing
 
 # -------fitness functions---------
 
@@ -27,33 +27,63 @@ def fitness_sphere(position):
     return fitness_value;
 
 
+# rosenbrock function
+def fitness_rosenbrock(position):
+    fitness_value = 0.0
+    for i in range(len(position)-1):
+        x1 = position[i]
+        x2 = position[i+1]
+        fitness_value += 100 * (x1 ** 2 - x2) ** 2 + (x2 - 1) ** 2
+    return fitness_value
+
+
+# zakharov function
+def fitness_zakharov(position):
+    part1 = 0.0
+    part2 = 0.0
+    for i in range(len(position)):
+        x1 = position[i]
+        part1 += x1**2
+        part2 += 0.5 * x1
+    fitness_value = part1 + part2 ** 2 + part2 ** 4
+    return fitness_value
+
+
+def fitness_levy(position):
+    position = np.asarray(position)
+    position_w = 1 + (position - 1) / 4
+    return (
+        np.sin(math.pi * position_w[0]) ** 2
+        + (position_w[-1] - 1) ** 2 * (1 + np.sin(2 * math.pi * position_w[-1]) ** 2)
+        + sum(
+            [
+                (wi - 1) ** 2 * (1 + 10 * np.sin(math.pi * wi + 1) ** 2)
+                for wi in position_w[:-1]
+            ]
+        )
+    )
+
 # -------------------------
+
 
 # Student class
 class Student:
-    def __init__(self, fitness, dim, minx, maxx, seed):
-        self.rnd = random.Random(seed)
+    def __init__(self, fitness, position):
 
-        # a list of size dim
-        # with 0.0 as value of all the elements
-        self.position = [0.0 for i in range(dim)]
-
-        # loop dim times and randomly select value of decision var
-        # value should be in between minx and maxx
-        for i in range(dim):
-            self.position[i] = ((maxx - minx) *
-                                self.rnd.random() + minx)
+        self.position = position
 
         # compute the fitness of student
         self.fitness = fitness(self.position)
 
 
 # Teaching learning based optimization
-def tlbo(fitness, max_iter, n, dim, minx, maxx):
-    rnd = random.Random(0)
+def tlbo(fitness, max_iter, population, boundaries):
+    minx, maxx = boundaries
+    n = len(population)
+    dim = len(population[0])
 
     # create n random students
-    classroom = [Student(fitness, dim, minx, maxx, i) for i in range(n)]
+    classroom = [Student(fitness, ind.tolist()) for ind in population]
 
     # compute the value of best_position and best_fitness in the classroom
     Xbest = [0.0 for i in range(dim)]
@@ -64,14 +94,14 @@ def tlbo(fitness, max_iter, n, dim, minx, maxx):
             Fbest = classroom[i].fitness
             Xbest = copy.copy(classroom[i].position)
 
-            # main loop of tlbo
+    # main loop of tlbo
     Iter = 0
     while Iter < max_iter:
 
         # after every 10 iterations
         # print iteration number and best fitness value so far
-        if Iter % 10 == 0 and Iter > 1:
-            print("Iter = " + str(Iter) + " best fitness = %.3f" % Fbest)
+        # if Iter % 10 == 0 and Iter > 1:
+            # print("Iter = " + str(Iter) + " best fitness = %.3f" % Fbest)
 
         # for each student of classroom
         for i in range(n):
@@ -92,14 +122,14 @@ def tlbo(fitness, max_iter, n, dim, minx, maxx):
 
             # teaching factor (TF)
             # either 1 or 2 ( randomly chosen)
-            TF = random.randint(1, 3)
+            TF = random.randint(1, 2)
 
             # best student of the class is teacher
             Xteacher = Xbest
 
             # compute new solution
             for j in range(dim):
-                Xnew[j] = classroom[i].position[j] + rnd.random() * (Xteacher[j] - TF * Xmean[j])
+                Xnew[j] = classroom[i].position[j] + random.random() * (Xteacher[j] - TF * Xmean[j])
 
             # if Xnew < minx OR Xnew > maxx
             # then clip it
@@ -135,11 +165,11 @@ def tlbo(fitness, max_iter, n, dim, minx, maxx):
             Xnew = [0.0 for i in range(dim)]
             if (classroom[i].fitness < Xpartner.fitness):
                 for j in range(dim):
-                    Xnew[j] = classroom[i].position[j] + rnd.random() * (
+                    Xnew[j] = classroom[i].position[j] + random.random() * (
                                 classroom[i].position[j] - Xpartner.position[j])
             else:
                 for j in range(dim):
-                    Xnew[j] = classroom[i].position[j] - rnd.random() * (
+                    Xnew[j] = classroom[i].position[j] - random.random() * (
                                 classroom[i].position[j] - Xpartner.position[j])
 
             # if Xnew < minx OR Xnew > maxx
@@ -174,61 +204,61 @@ def tlbo(fitness, max_iter, n, dim, minx, maxx):
 
 # ----------------------------
 # Driver code for rastrigin function
+if __name__ == '__main__':
+    print("\nBegin  teaching learning based optimization on rastrigin function\n")
+    dim = 3
+    fitness = fitness_rastrigin
 
-print("\nBegin  teaching learning based optimization on rastrigin function\n")
-dim = 3
-fitness = fitness_rastrigin
+    print("Goal is to minimize Rastrigin's function in " + str(dim) + " variables")
+    print("Function has known min = 0.0 at (", end="")
+    for i in range(dim - 1):
+        print("0, ", end="")
+    print("0)")
 
-print("Goal is to minimize Rastrigin's function in " + str(dim) + " variables")
-print("Function has known min = 0.0 at (", end="")
-for i in range(dim - 1):
-    print("0, ", end="")
-print("0)")
+    num_particles = 50
+    max_iter = 100
 
-num_particles = 50
-max_iter = 100
+    print("Setting num_particles = " + str(num_particles))
+    print("Setting max_iter    = " + str(max_iter))
+    print("\nStarting TLBO algorithm\n")
 
-print("Setting num_particles = " + str(num_particles))
-print("Setting max_iter    = " + str(max_iter))
-print("\nStarting TLBO algorithm\n")
+    best_position = tlbo(fitness, max_iter, num_particles, dim, -10.0, 10.0)
 
-best_position = tlbo(fitness, max_iter, num_particles, dim, -10.0, 10.0)
+    print("\nTLBO completed\n")
+    print("\nBest Student found:")
+    print(["%.6f" % best_position[k] for k in range(dim)])
+    fitness_value = fitness(best_position)
+    print("fitness of best Student = %.6f" % fitness_value)
 
-print("\nTLBO completed\n")
-print("\nBest Student found:")
-print(["%.6f" % best_position[k] for k in range(dim)])
-fitness_value = fitness(best_position)
-print("fitness of best Student = %.6f" % fitness_value)
+    print("\nEnd TLBO for rastrigin function\n")
 
-print("\nEnd TLBO for rastrigin function\n")
+    print()
+    print()
 
-print()
-print()
+    # Driver code for Sphere function
+    print("\nBegin teaching learning based optimization on sphere function\n")
+    dim = 3
+    fitness = fitness_sphere
 
-# Driver code for Sphere function
-print("\nBegin teaching learning based optimization on sphere function\n")
-dim = 3
-fitness = fitness_sphere
+    print("Goal is to minimize sphere function in " + str(dim) + " variables")
+    print("Function has known min = 0.0 at (", end="")
+    for i in range(dim - 1):
+        print("0, ", end="")
+    print("0)")
 
-print("Goal is to minimize sphere function in " + str(dim) + " variables")
-print("Function has known min = 0.0 at (", end="")
-for i in range(dim - 1):
-    print("0, ", end="")
-print("0)")
+    num_particles = 50
+    max_iter = 100
 
-num_particles = 50
-max_iter = 100
+    print("Setting num_particles = " + str(num_particles))
+    print("Setting max_iter    = " + str(max_iter))
+    print("\nStarting TLBO algorithm\n")
 
-print("Setting num_particles = " + str(num_particles))
-print("Setting max_iter    = " + str(max_iter))
-print("\nStarting TLBO algorithm\n")
+    best_position = tlbo(fitness, max_iter, num_particles, dim, -10.0, 10.0)
 
-best_position = tlbo(fitness, max_iter, num_particles, dim, -10.0, 10.0)
+    print("\nTLBO completed\n")
+    print("\nBest Student found:")
+    print(["%.6f" % best_position[k] for k in range(dim)])
+    fitness_value = fitness(best_position)
+    print("fitness of best Student = %.6f" % fitness_value)
 
-print("\nTLBO completed\n")
-print("\nBest Student found:")
-print(["%.6f" % best_position[k] for k in range(dim)])
-fitness_value = fitness(best_position)
-print("fitness of best Student = %.6f" % fitness_value)
-
-print("\nEnd TLBO for sphere function\n")
+    print("\nEnd TLBO for sphere function\n")
