27523dc03ad55f96149351c8fc43e3b2a1c7ad64
rafal.kusmierz.biz@gmail.com <r.kusmierz@stud.elka.pw.edu.pl>
2024-03-29 14:58:55 +0100

    cec test comments added

--- optimization_functions/cec/test.py
+++ optimization_functions/cec/test.py
@@ -8,9 +8,9 @@ Created on Sat Jan  1 17:14:04 2022
 import numpy as np
 from CEC2022 import cec2022_func
 
-nx = 10
-mx = 10
-fx_n = 12
+nx = 10     # dimensions - 2, 10, 20
+mx = 7     # population size - int
+fx_n = 12   # function - 1, 2, 3, ..., 11, 12
 
 CEC = cec2022_func(func_num = fx_n)
 
