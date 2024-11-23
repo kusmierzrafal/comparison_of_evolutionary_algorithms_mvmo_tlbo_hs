0536c6168d6552128f4fb7fb5b9ef9d9a8e12eeb
rafal.kusmierz.biz@gmail.com <r.kusmierz@stud.elka.pw.edu.pl>
2024-11-23 22:23:35 +0100

    feat: tuning_summary_tabs std column added

--- hyperparameters_tuning_summary_tabs.py
+++ hyperparameters_tuning_summary_tabs.py
@@ -51,20 +51,21 @@ tlbo_result_df["result"] = tlbo_result_df.apply(get_result_tlbo, axis=1)
 tlbo_stats_df = tlbo_result_df[["pop_size", "result"]].groupby("pop_size").min()
 tlbo_stats_df = tlbo_stats_df.merge(tlbo_result_df[["pop_size", "result"]].groupby("pop_size").max(), left_index=True, right_index=True)
 tlbo_stats_df = tlbo_stats_df.merge(tlbo_result_df[["pop_size", "result"]].groupby("pop_size").mean().round(2), left_index=True, right_index=True)
-tlbo_stats_df.columns = ["min_result", "max_result", "mean_result"]
+tlbo_stats_df = tlbo_stats_df.merge(tlbo_result_df[["pop_size", "result"]].groupby("pop_size").std().round(2), left_index=True, right_index=True)
+tlbo_stats_df.columns = ["min_result", "max_result", "mean_result", "std_result"]
 tlbo_stats_df = tlbo_stats_df.reset_index()
 
 
 tlbo_latex_table_start = f"""
-\\begin{{table}}[!h] \label{{tab:tabela1}} \centering
+\\begin{{table}}[H] \label{{tab:tabela1}} \centering
 \caption{{Dobór parametrów TLBO}}
-\\begin{{tabular}} {{| c | c | c | c |}} \hline
-    rozmiar populacji & najlepszy wynik & najsłabszy wynik & średni wynik \\\\ \hline
+\\begin{{tabular}} {{| c | c | c | c | c |}} \hline
+    rozmiar populacji & najlepszy wynik & najsłabszy wynik & średni wynik & odchylenie \\\\ \hline
     
 """
 
 tlbo_latex_body_table = """
-{pop_size} & {min_result} & {max_result} & {mean_result} \\\\ \hline
+{pop_size} & {min_result} & {max_result} & {mean_result} & {std_result} \\\\ \hline
 """
 
 latex_table_end = f"""
@@ -76,7 +77,7 @@ latex_table_end = f"""
 tlbo_latex_table = ""
 tlbo_latex_table += tlbo_latex_table_start
 for row in tlbo_stats_df.iterrows():
-    tlbo_latex_table += tlbo_latex_body_table.format(pop_size=int(row[1]["pop_size"]), min_result=row[1]["min_result"], max_result=row[1]["max_result"], mean_result=row[1]["mean_result"])
+    tlbo_latex_table += tlbo_latex_body_table.format(pop_size=int(row[1]["pop_size"]), min_result=row[1]["min_result"], max_result=row[1]["max_result"], mean_result=row[1]["mean_result"], std_result=row[1]["std_result"])
 tlbo_latex_table += latex_table_end
 
 print(tlbo_latex_table)
@@ -90,26 +91,28 @@ mvmo_result_df["result"] = mvmo_result_df.apply(get_result_mvmo, axis=1)
 mvmo_stats_df = mvmo_result_df[["n_best_size", "result"]].groupby("n_best_size").min()
 mvmo_stats_df = mvmo_stats_df.merge(mvmo_result_df[["n_best_size", "result"]].groupby("n_best_size").max(), left_index=True, right_index=True)
 mvmo_stats_df = mvmo_stats_df.merge(mvmo_result_df[["n_best_size", "result"]].groupby("n_best_size").mean().round(2), left_index=True, right_index=True)
-mvmo_stats_df.columns = ["min_result", "max_result", "mean_result"]
+mvmo_stats_df = mvmo_stats_df.merge(mvmo_result_df[["n_best_size", "result"]].groupby("n_best_size").std().round(2), left_index=True, right_index=True)
+mvmo_stats_df.columns = ["min_result", "max_result", "mean_result", "std_result"]
 mvmo_stats_df = mvmo_stats_df.reset_index()
 
 mvmo_latex_table_start = f"""
-\\begin{{table}}[!h] \label{{tab:tabela1}} \centering
+\\begin{{table}}[H] \label{{tab:tabela1}} \centering
 \caption{{Dobór parametrów MVMO}}
-\\begin{{tabular}} {{| c | c | c | c |}} \hline
-    rozmiar archiwum & najlepszy wynik & najsłabszy wynik & średni wynik \\\\ \hline
+\\begin{{tabular}} {{| c | c | c | c | c |}} \hline
+    rozmiar archiwum & najlepszy wynik & najsłabszy wynik & średni wynik & odchylenie \\\\ \hline
 
 """
 
 mvmo_latex_body_table = """
-{n_best_size} & {min_result} & {max_result} & {mean_result} \\\\ \hline
+{n_best_size} & {min_result} & {max_result} & {mean_result} & {std_result} \\\\ \hline
 """
 
 mvmo_latex_table = ""
 mvmo_latex_table += mvmo_latex_table_start
 for row in mvmo_stats_df.iterrows():
     mvmo_latex_table += mvmo_latex_body_table.format(n_best_size=int(row[1]["n_best_size"]), min_result=row[1]["min_result"],
-                                                     max_result=row[1]["max_result"], mean_result=row[1]["mean_result"])
+                                                     max_result=row[1]["max_result"], mean_result=row[1]["mean_result"],
+                                                     std_result=row[1]["std_result"])
 mvmo_latex_table += latex_table_end
 
 print(mvmo_latex_table)
